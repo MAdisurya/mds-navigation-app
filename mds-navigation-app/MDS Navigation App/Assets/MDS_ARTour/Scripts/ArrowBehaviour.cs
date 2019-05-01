9a95904c72554b42ce278756839dd33be986adf6
Mario Adisurya <>
2019-05-01 12:02:01 +1200

    Initial implementation of ArrowBehaviour
    Algorithm for ArrowBehaviour.LookAt() still not working as intended, but very close

---
+++ mds-navigation-app/MDS Navigation App/Assets/MDS_ARTour/Scripts/ArrowBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARTour
+{
+    public class ArrowBehaviour : MonoBehaviour
+    {
+        public int _defaultRotation = 0;    // The default rotation of the arrow sprite
+
+        void Update()
+        {
+            if (MainController.Instance.GetNodeController().TargetNode != null)
+            {
+                LookAt(MainController.Instance.GetNodeController().TargetNode.transform);
+            }
+        }
+
+        private void LookAt(Transform target)
+        {
+            Vector2 targetVPPos = Camera.main.WorldToViewportPoint(target.position);
+
+            float angle = Mathf.Atan2(targetVPPos.y, targetVPPos.x) * 90;
+
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
