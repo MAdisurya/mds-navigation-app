84512383abb1927308c9353182d8477dcfb2677b
Mario Adisurya <>
2019-05-14 10:42:31 +1200

    + INodeListener interface

---
+++ mds-navigation-app/MDS Navigation App/Assets/MDS_ARTour/Scripts/Interfaces/INodeListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARTour
+{
+    public interface INodeListener
+    {
+        void OnTargetNodeChanged(MDSNode newTargetNode);     // Callback for when target node has been changed
+    } 
+}
